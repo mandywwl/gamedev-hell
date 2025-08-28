3cee1e6df22b30556f3acbc9b752658191caa829
hjlau002@mymail.sim.edu.sg <>
2025-08-28 23:52:14 +0800

    Added Boss Area, Boss and Trigger

---
+++ Assets/Scripts/EnemyTrigger.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossEncounter : MonoBehaviour
+{
+    [SerializeField] string combatSceneName = "Combat";
+    [SerializeField] int bossIndex = 0;  // which enemy in BattleSystem.enemyPrefabs
+
+    bool triggered;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.CompareTag("Player"))
+            Debug.Log("Entered boss trigger!");
+
+        if (triggered) return;
+        if (!other.CompareTag("Player")) return;
+
+        triggered = true;
+
+        // pass data to battle
+        BattleTransfer.enemyIndex = bossIndex;
+        BattleTransfer.returnSceneName = SceneManager.GetActiveScene().name;
+        BattleTransfer.returnPosition = other.transform.position;
+
+        SceneManager.LoadScene(combatSceneName);
+    }
+
+    public static class BattleTransfer
+    {
+        public static string returnSceneName;
+        public static int enemyIndex = -1;
+        public static Vector3 returnPosition;
+    }
+}
