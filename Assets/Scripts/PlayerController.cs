f3d9cf6f38fd72270c0bd47d9df9fd08df0656a9
ljhlwq.noble6@gmail.com <>
2025-08-31 03:38:48 +0800

    With Changeset 71

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -176,8 +176,6 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-
-
     private void TryInteractWithChest()
     {
         if (nearbyChest != null && nearbyChest.CanInteract())
